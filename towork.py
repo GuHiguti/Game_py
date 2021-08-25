d98f067b0b93789c7acee74a648893703314a360
Gustavo Higuti de Souza <g250355@dac.unicamp.br>
2021-08-25 02:22:32 -0300

    Add music

--- towork.py
+++ towork.py
@@ -7,12 +7,21 @@ import math
 #inicializar pygame
 pygame.init()
 
+#música de fundo
+pygame.mixer.music.set_volume(0.1)
+background_music = pygame.mixer.music.load('Audio/Background1.mp3')
+pygame.mixer.music.play(-1)
+
+#som de colisão
+somzinho = pygame.mixer.Sound('Audio/Coin.wav')
+somzinho.set_volume(0.15)
+
 #variáveis
 width = 640
 height = 480
 
-xobj = width/2
-yobj = height/2
+xobj = int(width/2)
+yobj = int(height/2)
 
 xm = random.randint(0, 610)
 ym = random.randint(0, 450)
@@ -34,16 +43,16 @@ relogio = pygame.time.Clock()
 #loop principal
 while True:
     relogio.tick(60) #frames per second
-    scr.fill((0,0,0)) #clear screen
+    scr.fill((250,250,250)) #clear screen
     mensagem = f'Pontos: {pontos}'
-    texto = fonte.render(mensagem, True, (255,255,255)) #configurar texto
+    texto = fonte.render(mensagem, True, (0,0,0)) #configurar texto
 
     #Events loop
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-
+        
     #move object
     if pygame.key.get_pressed()[K_a]:
         right = False
@@ -78,15 +87,15 @@ while True:
 
 
     #formas na tela
-    ret_amarelo = pygame.draw.rect(scr, (255,205,0), (xobj,yobj,30,30)) #draw rectangle
-    ret_azul = pygame.draw.rect(scr,(0,0,255), (xm,ym,30,30))
+    ret_amarelo = pygame.draw.rect(scr, (0,255,0), (xobj,yobj,30,30)) #draw rectangle
+    ret_azul = pygame.draw.rect(scr,(255,0,0), (xm,ym,30,30))
 
     #configurar colisão
-    if ret_amarelo.colliderect(ret_azul):
-        print("colidiu")        
+    if ret_amarelo.colliderect(ret_azul): 
         xm = random.randint(0, 610)
         ym = random.randint(0, 450)
         pontos += 1
+        somzinho.play()
 
     #limites da tela
     if xobj + 15>= width:
